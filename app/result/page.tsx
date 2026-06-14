1939d7addfdcd4bae095ec7d81b59e174148c5de
哼哼哈嘿 <henghenghahei@liyanjun08deMacBook-Air.local>
2026-06-14 19:52:08 +0800

    fix import extension issue

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -1,19 +1,5 @@
-'use client'
+import ResultClient from './ResultClient'
 
-import { useSearchParams } from 'next/navigation'
-
-function ResultContent() {
-  const searchParams = useSearchParams()
-  const data = searchParams.get('data')
-
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>你的测试结果</h1>
-      <p>你属于：{data ?? '未知生物'} 🐭</p>
-    </main>
-  )
-}
-
-export default function ResultPage() {
-  return <ResultContent />
+export default function Page() {
+  return <ResultClient />
 }
